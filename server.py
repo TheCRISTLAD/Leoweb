48e476031c13eeb5d5877bb0e527a6aa969f066c
Cristian Montero <72994114+TheCRISTLAD@users.noreply.github.com>
2021-04-12 09:56:39 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+@app.route('/', methods="['GET']")
+def index():
+  return app.send_static_file('index.html')
+
+@app.route('/login',methods="['GET']")
+def login():
+    return app.send_static_file('login.html')
+
+@app.route('/signup',methods="['GET']")
+def signup():
+    return app.send_static_file('register.html')
