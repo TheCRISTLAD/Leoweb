5d0b97c93714cf79c476eefb7112e6836ac14532
Cristian Montero <72994114+TheCRISTLAD@users.noreply.github.com>
2021-04-23 09:53:45 -0400

    Update server.py

--- server.py
+++ server.py
@@ -2,14 +2,22 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods="['GET']")
+@app.route('/', methods =['GET'])
 def index():
   return app.send_static_file('index.html')
 
-@app.route('/login',methods="['GET']")
+@app.route('/login',methods=['GET'])
 def login():
     return app.send_static_file('login.html')
 
-@app.route('/signup',methods="['GET']")
+@app.route('/processLogin', methods=['POST'])
+def process_login():
+  missing = []
+  fields = ['email', 'password', 'login_submit']
+
+@app.route('/signup',methods=['GET'])
 def signup():
     return app.send_static_file('register.html')
+
+if __name__ == '__main__':
+  app.run(debug = True, port = 80)
