07f12de7c1a450266f45ff36f4c693811d6948b7
Cristian Montero <72994114+TheCRISTLAD@users.noreply.github.com>
2021-05-14 09:45:52 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 from flask import Flask, request, url_for
 
-#Inicio de rutas
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
     return app.send_static_file('index.html')
-#Esta cosa me manda al inicio
+
 
 @app.route('/Login', methods=['GET', 'POST'])
 def Login():
@@ -22,11 +21,10 @@ def Login():
             return load_user(request.form['email'], request.form['pswd'])
 
     return app.send_static_file('Login.html')
-#Este pana me manda al lugar donde robare tus datos
+
 
 @app.route('/processlogin', methods=['POST'])
 def process_login():
-    #Esto servira para evaluar los datos
     missing = []
     fields = ['email', 'pswd', 'login']
     for field in fields:
@@ -43,7 +41,7 @@ def process_login():
 
 def registro(): 
     return app.send_static_file('registro.html')
-#Aqui puedes ingresar tu tarjeta credito, dale dale, no pasara nada
+
 
 #Fin de rutas
 
