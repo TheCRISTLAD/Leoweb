693e6f85de38b901348d19a16a31ed7c302b0015
Cristian Montero <72994114+TheCRISTLAD@users.noreply.github.com>
2021-05-14 08:49:39 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,23 +1,51 @@
-from flask import Flask, request
+from flask import Flask, request, url_for
 
+#Inicio de rutas
 app = Flask(__name__)
 
-@app.route('/', methods =['GET'])
+@app.route('/', methods=['GET','POST'])
 def index():
-  return app.send_static_file('index.html')
+    return app.send_static_file('index.html')
+#Esta cosa me manda al inicio
 
-@app.route('/login',methods=['GET'])
-def login():
-    return app.send_static_file('login.html')
+@app.route('/Login', methods=['GET', 'POST'])
+def Login():
+    if request.method == 'POST':
+        missing = []
+        fields = ['email', 'pswd', 'login']
+        for field in fields:
+         value = request.form.get(field, None)
+        if value is None:
+            misssing.append(field)
+        if missing:
+            return render_template('MissingFields.html',next = url_for('Login.html'))
+            return load_user(request.form['email'], request.form['pswd'])
 
-@app.route('/processLogin', methods=['POST'])
+    return app.send_static_file('Login.html')
+#Este pana me manda al lugar donde robare tus datos
+
+@app.route('/processlogin', methods=['POST'])
 def process_login():
-  missing = []
-  fields = ['email', 'password', 'login_submit']
+    #Esto servira para evaluar los datos
+    missing = []
+    fields = ['email', 'pswd', 'login']
+    for field in fields:
+        value = request.form.get(field, None)
+        if value is None:
+            misssing.append(field)
+        if missing:
+            return "Advertencia: El valor del campo introducido no existe"
+            return app.send_static_file('process_login.html')
+            return load_user(request.form['email'], request.form['pswd'])
 
-@app.route('/signup',methods=['GET'])
-def signup():
-    return app.send_static_file('register.html')
 
-if __name__ == '__main__':
-  app.run(debug = True, port = 80)
+@app.route('/registro', methods=['GET','POST'])
+
+def registro(): 
+    return app.send_static_file('registro.html')
+#Aqui puedes ingresar tu tarjeta credito, dale dale, no pasara nada
+
+#Fin de rutas
+
+if __name__ == '__main__':  
+    app.run(debug = True, port = 80)
